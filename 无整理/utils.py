eb0b75094bf6d520589d8e876971abfcb48e77ab
Enoch <150331674+Ayanami-E@users.noreply.github.com>
2025-05-28 07:36:10 +0300

    Update utils.py

--- 无整理/utils.py
+++ 无整理/utils.py
@@ -3,7 +3,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.utils.data import Dataset
-
+#github上一个老哥的开源 可以用我就直接用了
 
 def normalize_A(A, symmetry=False):
     A = F.relu(A)
